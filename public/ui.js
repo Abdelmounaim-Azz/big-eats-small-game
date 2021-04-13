ecbbb5ebd7555556ac123d9fd4bec3f23ba380de
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-13 09:15:11 +0000

    name form submition

--- public/ui.js
+++ public/ui.js
@@ -2,8 +2,16 @@ let wHeight = $(window).height();
 let wWidth = $(window).width();
 let canvas = document.querySelector("#the-canvas");
 let context = canvas.getContext("2d");
+let player = {};
 canvas.width = wWidth;
 canvas.height = wHeight;
 $(window).load(() => {
   $("#loginModal").modal("show");
+});
+$(".name-form").submit((event) => {
+  event.preventDefault();
+  player.name = document.querySelector("#name-input").value;
+  $("#loginModal").modal("hide");
+  $("#spawnModal").modal("show");
+  document.querySelector(".player-name").innerHTML = player.name;
 });
