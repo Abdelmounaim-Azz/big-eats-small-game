f8021cc2f50aef731ada5377763ac64eb1c18d63
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-14 02:41:17 +0000

    game start generate orbs

--- io/socket.js
+++ io/socket.js
@@ -1,3 +1,10 @@
 const io = require("../servers").io;
 const orb = require("./classes/orb");
+let orbs = [];
+function initGame() {
+  for (let i = 0; i < settings.defaultOrbs; i++) {
+    orbs.push(new Orb(settings));
+  }
+}
+
 module.exports = io;
