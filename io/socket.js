94ee5e722cab4e2bb63c9ac57e1a416acfc243e8
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-14 02:45:30 +0000

    send orbs to connected clients

--- io/socket.js
+++ io/socket.js
@@ -1,6 +1,12 @@
 const io = require("../servers").io;
 const orb = require("./classes/orb");
 let orbs = [];
+initGame();
+io.sockets.on("connect", (socket) => {
+  socket.emit("initOrbs", {
+    orbs,
+  });
+});
 function initGame() {
   for (let i = 0; i < settings.defaultOrbs; i++) {
     orbs.push(new Orb(settings));
