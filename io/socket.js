96859a37f184af0da2e8bbda26885b9909098193
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-14 04:54:39 +0000

    create player object on io

--- io/socket.js
+++ io/socket.js
@@ -1,13 +1,20 @@
 const io = require("../servers").io;
 const Orb = require("./classes/orb");
+const Player = require("./classes/Player");
+const PlayerPayload = require("./classes/PlayerPayload");
+const PlayerConfig = require("./classes/PlayerConfig");
 let orbs = [];
 let settings = {
   defaultOrbs: 50,
   worldWidth: 500,
 };
+
 initGame();
 
 io.sockets.on("connect", (socket) => {
+  let playerConfig = new PlayerConfig(settings);
+  let playerPayload = new PlayerPayload(null, settings);
+  let player = new Player(socket.id, playerConfig, playerPayload);
   socket.emit("initOrbs", {
     orbs,
   });
