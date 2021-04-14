6b4b33c840432fd67b818cee2adea1425fe38c1b
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-04-14 03:45:24 +0000

    customize orb settings

--- io/socket.js
+++ io/socket.js
@@ -1,7 +1,12 @@
 const io = require("../servers").io;
-const orb = require("./classes/orb");
+const Orb = require("./classes/orb");
 let orbs = [];
 initGame();
+let settings = {
+  defaultOrbs: 50,
+  worldWidth: 500,
+};
+
 io.sockets.on("connect", (socket) => {
   socket.emit("initOrbs", {
     orbs,
